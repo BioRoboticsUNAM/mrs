1faadfad9d2566e32bceaafd0f386f7af01eb270
Luis Nava <luisnava@Ubuntu20.04.myguest.virtualbox.org>
2024-06-24 01:23:10 -0600

    Add install.sh script Add install.sh to .gitignore Update readme.MD

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Script of basic instalation
+sudo apt-get install ros-${ROS_DISTRO}-tf -y
+sudo apt-get install ros-${ROS_DISTRO}-image-transport -y
+sudo apt-get install ros-${ROS_DISTRO}-cv-bridge -y
